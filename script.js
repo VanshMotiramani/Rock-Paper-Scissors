75a99c65779d42c6e8a53919828dd9deba48f104
Vansh Motiramani <22052953@gmail.com>
2025-01-29 15:21:03 +0530

    Implement user's choice to play multiple rounds Added playGame function in script.js to implement the game for multiple rounds

--- script.js
+++ script.js
@@ -21,13 +21,63 @@ function getComputerChoice() {
 
     return "ERROR";
 }
-// let computerResult = getComputerChoice();
-console.log(getComputerChoice());
-
 // human choice
 function getHumanChoice() {
-    let humanChoice = prompt("Play your bet");
-    return humanChoice;
+    let humanResponse = prompt("Put up your choice: ");
+    return humanResponse;
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+let drawScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    console.log("Human Choice: " + humanChoice);
+    console.log("Computer Choice: " + computerChoice);
+    
+    if(humanChoice === "exit") {
+        return "exit";
+    } else if(!(humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissor")) {
+        return "Invalid selection";
+    } else if(humanChoice === computerChoice) {
+        drawScore++;
+        return "We Think Same, damn";
+    } else if((humanChoice === "rock" && computerChoice === "scissor") ||
+        (humanChoice === "scissor" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock")) {
+            humanScore++;
+            return "Smart HUH!!, You Won";
+    } else {
+        computerScore++;
+        return "I Made A better move, You LOSE";
+    }
+}
+
+function playGame() {
+    let numberOfRounds = parseInt(prompt("How many number of rounds"), 10);
+    while(numberOfRounds > 0) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        
+        let singleRound = playRound(humanSelection, computerSelection);
+        console.log(singleRound);
+
+        if (singleRound === "exit") {
+            break;
+        }
+
+        numberOfRounds--;
+    }
+
+    console.log("Your Score: " + humanScore);
+    console.log("Comp Score: " + computerScore);
+    console.log("Drawed Games: " + drawScore);
+}
+    
+playGame();
+
+// console.log("Human score: " + humanScore);
+// console.log("Computer Score: " + computerScore);
